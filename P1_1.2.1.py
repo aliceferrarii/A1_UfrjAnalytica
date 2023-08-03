10992455b03dfd94c1e47a71c1d8824c13349473
aliceferrarii <140724082+aliceferrarii@users.noreply.github.com>
2023-08-03 18:12:38 -0300

    Delete P1_1.2.1.py

--- P1_1.2.1.py
+++
@@ -1,6 +0,0 @@
-# Estruturas de controle 1
-Qualsuaequipefavorita = "Ufrj Analytica"
-if Qualsuaequipefavorita == "Ufrj Analytica":
-    print ("Minha equipe favorita é UFRJ ANALYTICA")
-else:
-    print ("Frase inesperada")
